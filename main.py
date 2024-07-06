09fcd7c7bc87d415ff0dbf6bf43962dfdd697928
Harinov <109693136+Harinov@users.noreply.github.com>
2024-07-06 08:23:03 +0000

    FISH 2 CORRECTION

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import streamlit as st
 from openai import OpenAI
 from PIL import Image
 import io
+import base64
+import requests
 
 # Setting up OpenAI API key
-client = OpenAI(api_key = st.secrets['OPENAI_SECRET_KEY'])
+client = OpenAI(api_key=st.secrets['OPENAI_SECRET_KEY'])
 
 st.title("Fish Identifier and Recipe Suggester")
 
@@ -30,26 +32,57 @@ if uploaded_file or picture:
 
     st.image(img, caption="Uploaded/Captured Image", use_column_width=True)
 
+
 def identify_fish_and_suggest_recipe(image):
     # Convert image to bytes
     img_bytes = io.BytesIO()
     image.save(img_bytes, format='JPEG')
     img_bytes = img_bytes.getvalue()
 
-    # Call OpenAI API for fish identification
-    response = client.chat.completions.create(
-      model="gpt-3.5-turbo",
-      messages=[
-        {"role": "system", "content": "fish name."},
-        {"role": "user", "content": "fish's description"},
-        {"role": "assistant", "content": "fish recipe"}
-      ]
-    ,
-    max_tokens=150
-        )
+    def encode_image(file_path):
+        with open(file_path, 'rb') as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+
+    def vision_file(file_path):
+        base64_image = encode_image(file_path)
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {userdata.get('OPENAI_API_KEY')}"
+        }
+
+        payload = {
+            "model":
+            "gpt-4o",
+            "messages": [{
+                "role":
+                "user",
+                "content": [{
+                    "type": "text",
+                    "text": "what is in this image?"
+                }, {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/png;base64,{base64_image}"
+                    }
+                }]
+            }],
+            "max_tokens":
+            20
+        }
+
+        response = requests.post("https://api.openai.com/v1/chat/completions",
+                                 headers=headers,
+                                 json=payload)
+
+        (response.json()['choices'][0]['message']['content'])
+
+    file_path = '/content/portraitt.png'
+    vision_file(file_path)
 
     fish_info = response.choices[0].message.content
     return fish_info
+
 
 if uploaded_file or picture:
     with st.spinner("Identifying fish and fetching recipe..."):
